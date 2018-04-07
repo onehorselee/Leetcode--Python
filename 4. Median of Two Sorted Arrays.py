e7451f6a141b0bf1b0a44487b9bfec857ef7759e
onehorselee <onehorselee@gmail.com>
2018-04-06 21:31:46 -0500

    Update 4. Median of Two Sorted Arrays.py

--- 4. Median of Two Sorted Arrays.py
+++ 4. Median of Two Sorted Arrays.py
@@ -1,51 +1,57 @@
 class Solution:
-def findMedianSortedArrays(self, A, B):
-    """
-    :type nums1: List[int]
-    :type nums2: List[int]
-    :rtype: float
-    """        
-    import statistics as stats
-    
-    if not A and B:
-        return stats.median(B)
-    if not B and A:
-        return stats.median(A)
-    if not A and not B:
-        return 0
-    
-    k = len(A) + len(B)
-    
-    if k % 2 != 0:                   # we need to find the (k + 1)/2 -1
-        med_index = int((k - 1)/2 )
-        odd_case = True
-    else:                        
-        med_index = int(k/2)         # we need to find k/2 and k/2 - 1
-        odd_case = False
+    def findMedianSortedArrays(self, A, B):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """        
+        import statistics as stats
+        
+        if not A and B:
+            return stats.median(B)
+        if not B and A:
+            return stats.median(A)
+        if not A and not B:
+            return 0
+        
+        k = len(A) + len(B)
+        
+        if k % 2 != 0:                   # we need to find the (k-1)/2
+            med_index = int((k - 1)/2) 
+            odd_case = True
+        else:                        
+            med_index = int(k/2)          # we need to find k/2 and k/2 - 1
+            odd_case = False
+            
+            
+        k -= 1
+        m = len(A) -1
+        n = len(B) -1
+        A = A + [0 for i in range(len(B))]
+
         
         
-    A = A + [0 for i in range(len(B))]
-    k -= 1
-    m = len(A) -1
-    n = len(B) -1
-    
-    
-    while m >= 0 and k >= med_index - 1:
-        if A[m] > B[n]:
-            A[k] = A[m]
-            k -=1
-            m -=1
-        else:
+        while m >= 0 and n >= 0 and k >= med_index - 1:
+            if A[m] > B[n]:
+                A[k] = A[m]
+                k -=1
+                m -=1
+            else:
+                A[k] = B[n]
+                k -=1
+                n -=1
+
+        while (n >= 0 and k >= med_index - 1):
             A[k] = B[n]
             k -=1
             n -=1
-
-    while n >= 0 and k >= med_index -1:
-        A[k] = B[n]
-        k -=1
-        n -=1
+                
+        if odd_case:
+            return A[med_index]
+        else:
+            return (A[med_index] + A[med_index -1])/2
+                
+                
             
-    if odd_case:
-        return A[med_index]
-    else:
-        return (A[med_index] + A[med_index -1])/2
+        
+        
