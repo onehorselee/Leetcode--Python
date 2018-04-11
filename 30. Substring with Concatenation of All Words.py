a18f77d835bafce76a0c049f217f30a1d53a2995
onehorselee <onehorselee@gmail.com>
2018-04-11 10:54:45 -0500

    Create 30. Substring with Concatenation of All Words.py

---
+++ 30. Substring with Concatenation of All Words.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+class Solution:
+    def findSubstring(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
+        
+        len_word = len(words[0])
+        len_combo = len_word * len(words)
+        if len(s) < len_combo:
+            return []
+        
+        result = []
+        dic = {w:words.count(w) for w in words}
+        # search words combinations in s
+        for i in range(len(s) - len_combo + 1):
+            seen = {defaultdict(int)}
+            start, count = i, len(words)
+            while count > 0:
+                substr = s[start: start + len_word]
+                # it is not in words list or it has more than the number of words
+                if (substr not in dic) or (dic[substr] == seen[substr]):  
+                    break
+                else:
+                    seen[substr] += 1
+                    start += len_word
+                    count -= 1
+            
+            if count == 0:
+                result.append(i)            
+    
+        return result
